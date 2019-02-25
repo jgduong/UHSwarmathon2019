22e4741c4cf2bef0d4997e2a5efec4fd153c0488
robertphu <43429776+robertphu@users.noreply.github.com>
2019-02-25 17:15:55 -0600

    add rviz

--- src/behaviours/src/ROSAdapter.cpp
+++ src/behaviours/src/ROSAdapter.cpp
@@ -27,6 +27,7 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/MultiArrayDimension.h>
 #include "swarmie_msgs/Waypoint.h"
+#include <visualization_msgs/Marker.h>
 
 // Include Controllers
 //#include "LogicController.h"
@@ -78,8 +79,35 @@ float sonarLeftData = 0.0;
 float sonarCenterData = 0.0;
 float sonarRightData = 0.0;
 
+uint32_t shape = visualization_msgs::Marker::CUBE;
+int swarmieid = 0;
+int markerid = 0;
+bool swarmieidset = false;
+
+template < typename T>
+int findInVector(const std::vector<T>  & vecOfElements, const T  & element)
+{
+	int result;
+ 
+	// Find given element in vector
+	auto it = std::find(vecOfElements.begin(), vecOfElements.end(), element);
+ 
+	if (it != vecOfElements.end())
+	{
+		result = distance(vecOfElements.begin(), it);
+	}
+	else
+	{
+		result = -1;
+	}
+ 
+	return result;
+}
+
 bool initialized = false;
 
+
+vector <string> names;
 geometry_msgs::Twist velocity;
 
 float linearVelocity = 0;	//forward speed, POSITIVE = forward, NEGATIVE = backward
@@ -96,8 +124,10 @@ ros::Publisher heartbeatPublisher;		//publishes ROSAdapters status via its "hear
 // Publishes swarmie_msgs::Waypoint messages on "/<robot>/waypooints"
 // to indicate when waypoints have been reached.
 ros::Publisher waypointFeedbackPublisher;	//publishes a waypoint to travel to if the rover is given a waypoint in manual mode
-ros::Publisher robotLocationGPS;			//publishes name of robot to /swarmies
+ros::Publisher robotnamePublisher;			//publishes name of robot to /swarmies
 ros::Publisher visitedLocationsPublisher;
+
+ros::Publisher marker_pub;
 
 // Subscribers
 ros::Subscriber joySubscriber;			//receives joystick information
@@ -237,7 +267,8 @@ int main(int argc, char **argv) {
   waypointFeedbackPublisher = mNH.advertise<swarmie_msgs::Waypoint>((publishedName + "/waypoints"), 1, true);		//publishes a waypoint to travel to if the rover is given a waypoint in manual mode
   visitedLocationsPublisher = mNH.advertise<std_msgs::Float32MultiArray>(("/visitedLocation"), 10, true);
 	
-  robotLocationGPS = mNH.advertise<geometry_msgs::Point>(("/swarmies"), 10, true);						//publishes robotnames to /swarmies
+  robotnamePublisher = mNH.advertise<std_msgs::String>(("/swarmies"), 10, true);						//publishes robotnames to /swarmies						//publishes robotnames to /swarmies
+  marker_pub = mNH.advertise<visualization_msgs::Marker>("/visualization_marker", 1);
 
   //timers
   publish_status_timer = mNH.createTimer(ros::Duration(1), publishStatusTimerEventHandler);
@@ -998,7 +1029,6 @@ void behaviourStateMachine(const ros::TimerEvent&)
 			geometry_msgs::Point temp;
 			temp.x = currentLocationMap.x;
 			temp.y = currentLocationMap.y;
-			robotLocationGPS.publish(temp);
 			  //SET the centerMap location by passing that variable here		
 
 		      //startTime = getROSTimeInMilliSecs();
@@ -1405,6 +1435,52 @@ void visitedLocationsHandler(const std_msgs::Float32MultiArray::ConstPtr& msg) {
 	float x = msg->data[0];
 	float y = msg->data[1];
 	visitedLocations[x].insert(y);
+
+	//check if swarmieid is set
+	if (!swarmieidset && swarmieid != 0){
+		markerid = swarmieid * 10000000;
+		swarmieidset = true;
+	} 
+
+	visualization_msgs::Marker marker;
+   	// Set the frame ID and timestamp.  See the TF tutorials for information on these.
+   	marker.header.frame_id = publishedName + "/map";
+   	marker.header.stamp = ros::Time::now();
+
+    	// Set the namespace and id for this marker.  This serves to create a unique ID
+    	// Any marker sent with the same namespace and id will overwrite the old one
+   	marker.ns = "basic_shapes";
+   	marker.id = markerid;
+    	// Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+    	marker.type = shape;
+
+	// Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+	    marker.action = visualization_msgs::Marker::ADD;
+
+	// Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+	marker.pose.position.x = x;
+	marker.pose.position.y = y;
+	marker.pose.position.z = 0;
+	marker.pose.orientation.x = 0.0;
+	marker.pose.orientation.y = 0.0;
+	marker.pose.orientation.z = 0.0;
+	marker.pose.orientation.w = 1.0;
+
+	// Set the scale of the marker -- 1x1x1 here means 1m on a side
+	marker.scale.x = 0.25;
+	marker.scale.y = 0.25;
+	marker.scale.z = 0.25;
+	// Set the color -- be sure to set alpha to something non-zero!
+	marker.color.r = 0.0;
+	marker.color.g = 1.0;
+	marker.color.b = 0.0;
+	marker.color.a = 1.0;
+
+	marker.lifetime = ros::Duration();
+
+	marker_pub.publish(marker);
+	markerid = markerid + 1;
+
 	//visitedLocations[myCoordinate[0]].insert(myCoordinate[1]);
 	//visitedLocationsPublisher.publish(myCoordinate);
 }
@@ -1487,7 +1563,7 @@ void publishStatusTimerEventHandler(const ros::TimerEvent&)
   status_publisher.publish(msg);
 
   msg.data = publishedName;
-  //robotnames.publish(msg);
+  robotnamePublisher.publish(msg);
 }
 
 void manualWaypointHandler(const swarmie_msgs::Waypoint& message)
