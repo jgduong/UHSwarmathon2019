bfa68e279cb11b135fec263a6663a61c155691ac
jgduong <jennyduong1996@gmail.com>
2019-03-04 16:04:08 -0600

    testing SpiralSearchController.h

---
+++ src/behaviours/src/SpiralSearchController.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class SpiralSearchController {
+
+  public: 
+    
+    void DoWork() {
+      cout << "Spiral Search Controller is working" << endl;
+    }
+
+
+};
