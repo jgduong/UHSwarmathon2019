8598ff573b63347367263930156971d8a9b92cfa
jgduong <jennyduong1996@gmail.com>
2019-03-04 17:04:35 -0600

    Update SpiralSearchController.h

--- src/behaviours/src/SpiralSearchController.h
+++ src/behaviours/src/SpiralSearchController.h
@@ -6,7 +6,7 @@ class SpiralSearchController {
 
   public: 
     
-    void DoWork(swarmie *thisSwarmie) {
+    void DoWork(swarmie thisSwarmie) {
       cout << "Spiral Search Controller is working" << endl;
       cout << "Current Location: (" << thisSwarmie->currX << ", " << thisSwarmie->currY << ")" << endl;
       cout << "Theta = " << thisSwarmie->currTheta << endl;
