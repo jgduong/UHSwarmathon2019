1b33b5c937be3e30a76ff3919bc1474e06801c6b
jgduong <jennyduong1996@gmail.com>
2019-03-05 16:21:02 -0600

    Update LogicController.h

--- src/behaviours/src/LogicController.h
+++ src/behaviours/src/LogicController.h
@@ -97,143 +97,144 @@ class LogicController {
   
     	LogicController(float initialX, float initialY, float initialTheta) {
 	    
-       	prevState = 0;
-       	currState = 0;
-	    
-	initialPosX = initialX;
-	initialPosY = initialY;
-       	startingTheta = initialTheta;
-       	currX = initialX;
-	currY = initialY;
-	currTheta = initialTheta;
-    
-       	Wheels.left = 0.0;
-       	Wheels.right = 0.0;
-    }
+		prevState = 0;
+		currState = 0;
 
-  void DoWork(int state) {
-    /*
-    Controller *currController;
-    for (int i = ControllerQueue.size(); i > 0; i--) {
-        currController = ControllerQueue.pop();
-        currController.DoWork();
-    }
-    */
-    if (state == SPIRAL_SEARCH) {
-      spiralSearchController.DoWork(currX, currY, currTheta);
-    }
-    else if (state == AVOID_OBSTACLE) {
-      //ObstacleController.DoWork();
-    }
-    else if (state == PICKUP) {
-      //PickupController.DoWork();
-    }
-    else if (state == DROPOFF) {
-      // DropoffController.DoWork();
-    }
-    else if (state == FIND_SPIRAL_EDGE) {
-      //FindEdgeController.DoWork();
-    }
-  }
-  
-  void updateInfo(float x, float y, float theta) {
-    currX = x;
-    currY = y;
-    currTheta = theta;
-  }
-  
-  void setCenterOffset(float x, float y) {
-    centerOffsetX = x;
-    centerOffsetY = y;
-  }
-  
-  struct wheels InitialRotate() {
-		//Rotate to starting position...
-	  float ninetyRotate = currTheta;
-	  cout << "Current theta is: " << currTheta << endl;
-		/*
-	  std_msgs::Float32MultiArray myCoordinate;
-		myCoordinate.layout.dim.push_back(std_msgs::MultiArrayDimension());
-		myCoordinate.layout.dim[0].size = 2;
-		myCoordinate.layout.dim[0].stride = 1;
-		myCoordinate.layout.dim[0].label = "poop";
-		
-		myCoordinate.data.push_back(normalizedValue(thisSwarmie->currX+centerOffsetX));
-		myCoordinate.data.push_back(normalizedValue(thisSwarmie->currY+centerOffsetY));
-		
-		visitedLocations[myCoordinate.data[0]].insert(myCoordinate.data[1]);
+		initialPosX = initialX;
+		initialPosY = initialY;
+		startingTheta = initialTheta;
+		currX = initialX;
+		currY = initialY;
+		currTheta = initialTheta;
 
-		visitedLocationsPublisher.publish(myCoordinate);
-	  */
-		cout << "step 2: rotating 90 degrees left..." << endl;
-		
-		geometry_msgs::Point tempLocal;
-		
-		float turnSize = 1.5;
-		bool exceedMag = false;
-
-		//if the ninety degree turn crosses the pi line, this is a special condition
-		if (abs(startingTheta + turnSize) >= 3.14159)
-		{
-			exceedMag = true;
-		}
-		cout << "exceed magnitude value is " << exceedMag << endl;
-		if (exceedMag)
-		{
-			float desiredTheta = 0.0;
-
-			desiredTheta = -3.142 + (startingTheta - turnSize);
-			if (currTheta >= desiredTheta && currTheta < 0.0)
-			{
-				//sendDriveCommand(0.0, 0.0);
-          			Wheels.left = 0.0;
-          			Wheels.right = 0.0;
-				//rotateBool = false;
-				//hardcodedPop = true;
-				//initialMove = true;
-				//step = 1;
-				initialPosX = currX;
-				initialPosY = currY;
-					
-				cout << "done rotating" << endl;
-			}
-			else {
-				//sendDriveCommand(-30.0, 30.0);
-          			Wheels.left = -30.0;
-          			Wheels.right = 30.0;
-				cout << "still rotating to calculated desired theta: " << desiredTheta << endl;
-			}
-				
-				
-				
-		}
-		else
-		{	//ninetyRotate = current theta
-			//if the diff between the current theta and starting theta is >= 90 degrees, stop
-			if (abs(ninetyRotate - startingTheta) >= 1.5)
-			{
-				//sendDriveCommand(0.0, 0.0);
-              			Wheels.left = 0.0;
-              			Wheels.right = 0.0;
-				//    rotateBool = false;
-				//hardcodedPop = true;
-				//initialMove = true;
-				//GPSCenter = true;
-				//step = 1;
-				initialPosX = currX;
-				initialPosY = currY;
-				cout << "done rotating" << endl;
-
-			 }	//else, turn right
-			 else {
-				//sendDriveCommand(-30.0, 30.0);
-              			Wheels.left = -30.0;
-              			Wheels.right = 30.0;
-			 }
-			
+		Wheels.left = 0.0;
+		Wheels.right = 0.0;
     	}
-  	return Wheels;
-  }
+
+	  void DoWork(int state) {
+	    /*
+	    Controller *currController;
+	    for (int i = ControllerQueue.size(); i > 0; i--) {
+		currController = ControllerQueue.pop();
+		currController.DoWork();
+	    }
+	    */
+	    if (state == SPIRAL_SEARCH) {
+	      spiralSearchController.DoWork(currX, currY, currTheta);
+	    }
+	    else if (state == AVOID_OBSTACLE) {
+	      //ObstacleController.DoWork();
+	    }
+	    else if (state == PICKUP) {
+	      //PickupController.DoWork();
+	    }
+	    else if (state == DROPOFF) {
+	      // DropoffController.DoWork();
+	    }
+	    else if (state == FIND_SPIRAL_EDGE) {
+	      //FindEdgeController.DoWork();
+	    }
+	  }
+
+	  void updateData(float x, float y, float theta) {
+	    	currX = x;
+	    	currY = y;
+	    	currTheta = theta;
+		spiralSearchController.updateData(x, y, theta);
+	  }
+
+	  void setCenterOffset(float x, float y) {
+	    centerOffsetX = x;
+	    centerOffsetY = y;
+	  }
+
+	  struct wheels InitialRotate() {
+			//Rotate to starting position...
+		  float ninetyRotate = currTheta;
+		  cout << "Current theta is: " << currTheta << endl;
+			/*
+		  std_msgs::Float32MultiArray myCoordinate;
+			myCoordinate.layout.dim.push_back(std_msgs::MultiArrayDimension());
+			myCoordinate.layout.dim[0].size = 2;
+			myCoordinate.layout.dim[0].stride = 1;
+			myCoordinate.layout.dim[0].label = "poop";
+
+			myCoordinate.data.push_back(normalizedValue(thisSwarmie->currX+centerOffsetX));
+			myCoordinate.data.push_back(normalizedValue(thisSwarmie->currY+centerOffsetY));
+
+			visitedLocations[myCoordinate.data[0]].insert(myCoordinate.data[1]);
+
+			visitedLocationsPublisher.publish(myCoordinate);
+		  */
+			cout << "step 2: rotating 90 degrees left..." << endl;
+
+			geometry_msgs::Point tempLocal;
+
+			float turnSize = 1.5;
+			bool exceedMag = false;
+
+			//if the ninety degree turn crosses the pi line, this is a special condition
+			if (abs(startingTheta + turnSize) >= 3.14159)
+			{
+				exceedMag = true;
+			}
+			cout << "exceed magnitude value is " << exceedMag << endl;
+			if (exceedMag)
+			{
+				float desiredTheta = 0.0;
+
+				desiredTheta = -3.142 + (startingTheta - turnSize);
+				if (currTheta >= desiredTheta && currTheta < 0.0)
+				{
+					//sendDriveCommand(0.0, 0.0);
+					Wheels.left = 0.0;
+					Wheels.right = 0.0;
+					//rotateBool = false;
+					//hardcodedPop = true;
+					//initialMove = true;
+					//step = 1;
+					initialPosX = currX;
+					initialPosY = currY;
+
+					cout << "done rotating" << endl;
+				}
+				else {
+					//sendDriveCommand(-30.0, 30.0);
+					Wheels.left = -30.0;
+					Wheels.right = 30.0;
+					cout << "still rotating to calculated desired theta: " << desiredTheta << endl;
+				}
+
+
+
+			}
+			else
+			{	//ninetyRotate = current theta
+				//if the diff between the current theta and starting theta is >= 90 degrees, stop
+				if (abs(ninetyRotate - startingTheta) >= 1.5)
+				{
+					//sendDriveCommand(0.0, 0.0);
+					Wheels.left = 0.0;
+					Wheels.right = 0.0;
+					//    rotateBool = false;
+					//hardcodedPop = true;
+					//initialMove = true;
+					//GPSCenter = true;
+					//step = 1;
+					initialPosX = currX;
+					initialPosY = currY;
+					cout << "done rotating" << endl;
+
+				 }	//else, turn right
+				 else {
+					//sendDriveCommand(-30.0, 30.0);
+					Wheels.left = -30.0;
+					Wheels.right = 30.0;
+				 }
+
+		}
+		return Wheels;
+	  }
 };
 
 #endif // LOGICCONTROLLER_H
