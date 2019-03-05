ec0cc9c9b075565cca43e541de473ae92e750749
jgduong <jennyduong1996@gmail.com>
2019-03-05 13:09:39 -0600

    Update LogicController.h

--- src/behaviours/src/LogicController.h
+++ src/behaviours/src/LogicController.h
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <unordered_map>
+#include <math.h>
+
+#include <std_msgs/Float32MultiArray.h>
+#include <geometry_msgs/Point.h>
 
 using namespace std;
 
@@ -56,6 +61,32 @@ typedef struct swarmie {
   }
 } Swarmie;
 
+struct wheels {
+  float left;
+  float right;
+}
+
+float calcDistance(float curX, float curY, float goalX, float goalY) {
+	float dist = sqrt( (goalX - curX)*(goalX - curX) + (goalY - curY)*(goalY - curY) );
+	return dist;
+}
+
+float normalizedValue(float x)
+{
+	float temp = x * 100;
+	int val = round(temp);
+	
+	if ((val % 25) > 13)
+	{
+		int n = (val/25) + 1;
+		return n*0.25;
+	}
+	else
+	{
+		int n = (val/25);
+		return n*0.25;
+	}
+}
 //extern Swarmie thisSwarmie;
 
 //typedef struct swarmie Swarmie;
@@ -65,9 +96,14 @@ class LogicController {
     SpiralSearchController spiralSearchController;
   
   public: 
+    float initialPosX;
+    float initialPosY;
+    float centerOffsetX;
+    float centerOffsetY;
     int prevState;
     int currState;
     Swarmie *thisSwarmie;
+    struct wheels Wheels;
     //priority_queue<Controller> ControllerQueue;
   
     LogicController() {}
@@ -77,24 +113,11 @@ class LogicController {
        currState = 0;
        Swarmie *tempSwarmie = new swarmie(initialX, initialY, initialTheta);
        thisSwarmie = tempSwarmie;
+    
+       Wheels.left = 0.0;
+       Wheels.right = 0.0;
     }
-/*  
-    void updateQueue(int state) {
-      //add obstacle controller
-       if (state == SPIRAL_SEARCH) {
-         ControllerQueue.push((Controller*)(&SpiralSearchController), 5);
-       }
-      else if (state == PICKUP) {
-        //add pickup controller
-      }
-      else if (state == DROPOFF) {
-         //add dropoff controller
-      }
-      else if (state == FIND_SPIRAL_EDGE) {
-        //ADD FIND_SPIRAL_EDGE CONTROLLER 
-      }
-    }
-*/  
+
   void DoWork(int state) {
     /*
     Controller *currController;
@@ -126,6 +149,98 @@ class LogicController {
     thisSwarmie->currTheta = theta;
   }
   
+  void setCenterOffset(float x, float y) {
+    centerOffsetX = x;
+    centerOffsetY = y;
+  }
+  
+  struct wheels InitialRotate() {
+		//Rotate to starting position...
+	  float ninetyRotate = thisSwarmie->currTheta;
+	  cout << "Current theta is: " << thisSwarmie->currTheta << endl;
+		/*
+	  std_msgs::Float32MultiArray myCoordinate;
+		myCoordinate.layout.dim.push_back(std_msgs::MultiArrayDimension());
+		myCoordinate.layout.dim[0].size = 2;
+		myCoordinate.layout.dim[0].stride = 1;
+		myCoordinate.layout.dim[0].label = "poop";
+		
+		myCoordinate.data.push_back(normalizedValue(thisSwarmie->currX+centerOffsetX));
+		myCoordinate.data.push_back(normalizedValue(thisSwarmie->currY+centerOffsetY));
+		
+		visitedLocations[myCoordinate.data[0]].insert(myCoordinate.data[1]);
+
+		visitedLocationsPublisher.publish(myCoordinate);
+	  */
+		cout << "step 2: rotating 90 degrees left..." << endl;
+		
+		geometry_msgs::Point tempLocal;
+		
+			float turnSize = 1.5;
+			bool exceedMag = false;
+
+			ninetyRotate = thisSwarmie->currTheta;
+			//if the ninety degree turn crosses the pi line, this is a special condition
+			if (abs(startingTheta + turnSize) >= 3.14159)
+			{
+				exceedMag = true;
+			}
+			cout << "exceed magnitude value is " << exceedMag << endl;
+			if (exceedMag)
+			{
+				float desiredTheta = 0.0;
+
+				desiredTheta = -3.142 + (startingTheta - turnSize);
+				if (thisSwarmie->theta >= desiredTheta && thisSwarmie->theta < 0.0)
+				{
+					//sendDriveCommand(0.0, 0.0);
+          thisSwarmie->leftVel = 0.0;
+          thisSwarmie->rightVel = 0.0;
+					rotateBool = false;
+					//hardcodedPop = true;
+				    	 //initialMove = true;
+				      //step = 1;
+					initialPosX = thisSwarmie->currX;
+					initialPosY = thisSwarmie->currY;
+					
+				     cout << "done rotating" << endl;
+				}
+				else {
+					//sendDriveCommand(-30.0, 30.0);
+          thisSwarmie->leftVel = -30.0;
+          thisSwarmie->rightVel = 30.0;
+					cout << "still rotating to calculated desired theta: " << desiredTheta << endl;
+				}
+				
+				
+				
+			}
+			else
+			{	//ninetyRotate = current theta
+				//if the diff between the current theta and starting theta is >= 90 degrees, stop
+			      if (abs(ninetyRotate - startingTheta) >= 1.5)
+			      {
+				      //sendDriveCommand(0.0, 0.0);
+              thisSwarmie->leftVel = 0.0;
+              thisSwarmie->rightVel = 0.0;
+					    rotateBool = false;
+				      hardcodedPop = true;
+				    	//initialMove = true;
+				      //GPSCenter = true;
+				      //step = 1;
+					    initialPosX = thisSwarmie->currX;
+					    initialPosY = thisSwarmie->currY;
+				      cout << "done rotating" << endl;
+
+			      }	//else, turn right
+			      else {
+				      //sendDriveCommand(-30.0, 30.0);
+              thisSwarmie->leftVel = -30.0;
+              thisSwarmie->rightVel = 30.0;
+			      }
+			
+    }
+  return Wheels;
 };
 
 #endif // LOGICCONTROLLER_H
