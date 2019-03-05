70745935987657667fb3cba0587f76186dd9c32b
jgduong <jennyduong1996@gmail.com>
2019-03-05 16:23:11 -0600

    Update SpiralSearchController.h

--- src/behaviours/src/SpiralSearchController.h
+++ src/behaviours/src/SpiralSearchController.h
@@ -5,6 +5,23 @@ using namespace std;
 class SpiralSearchController {
 
   public: 
+    float currX;
+    float currY;
+    float currTheta;
+  
+    float centerOffsetX;
+    float centerOffsetY;
+  
+    void updateData(float x, float y, float theta) {
+      currX = x;
+      currY = y;
+      currTheta = theta;
+    }
+  
+    void setCenterOffset(float x, float y) {
+      centerOffsetX = x;
+      centerOffsetY = y;
+    }
     
     void DoWork(float currX, float currY, float currTheta) {
       cout << "Spiral Search Controller is working" << endl;
