6672206fb4b45a74bea086f9e4ea0fd5461a5387
jgduong <jennyduong1996@gmail.com>
2019-03-05 13:49:26 -0600

    Update LogicController.h

--- src/behaviours/src/LogicController.h
+++ src/behaviours/src/LogicController.h
@@ -247,8 +247,8 @@ class LogicController {
 			      }
 			
     }
-	Wheels->left = thisSwarmie->leftVel;
-	  Wheels->right = thisSwarmie->rightVel;
+	Wheels.left = thisSwarmie->leftVel;
+	  Wheels.right = thisSwarmie->rightVel;
   	return Wheels;
   }
 };
