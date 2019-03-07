e588753387afb4d7c9a2de7834b4c1a385a0a776
jgduong <jennyduong1996@gmail.com>
2019-03-06 18:56:47 -0600

    Update SpiralSearchController.h

--- src/behaviours/src/SpiralSearchController.h
+++ src/behaviours/src/SpiralSearchController.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+#include <set>
 #include "LogicController.h"
 using namespace std;
 
@@ -11,6 +13,7 @@ class SpiralSearchController {
   
     float centerOffsetX;
     float centerOffsetY;
+    struct wheels Wheels;
   
     void updateData(float x, float y, float theta) {
       currX = x;
@@ -22,11 +25,36 @@ class SpiralSearchController {
       centerOffsetX = x;
       centerOffsetY = y;
     }
+
+    bool isVisited(float x, float y) {
+    if (visitedLocations.find(x) != visitedLocations.end()) {
+      //x location exists in hashmap, check y coordinate
+      if (visitedLocations[x].find(y) != visitedLocations[x].end()) {
+        //y location also exists, so this coordinate has been visited
+        return true;
+      }
+      else {
+          return false;	
+        }
+      }
+      else {
+        return false;
+      }
+    }
     
-    void DoWork(float currX, float currY, float currTheta) {
-      cout << "Spiral Search Controller is working" << endl;
-      cout << "Current Location: (" << currX << ", " << currY << ")" << endl;
-      cout << "Theta = " << currTheta << endl;
+    struct wheels DoWork(hashmap &visitedLocations) {
+      cout << "test: current location (" << currX << ", " << currY << ")";
+      if (visitedLocations.find(currX) != visitedLocations.end() && visitedLocations[currX].find(currY) != visitedLocations[currX].end())
+      {
+        cout << "exists in the hashmap" << endl;
+      }
+      else cout << "does not exist in the hashmap" << endl;
+      //cout << "Spiral Search Controller is working" << endl;
+      //cout << "Current Location: (" << currX << ", " << currY << ")" << endl;
+      //cout << "Theta = " << currTheta << endl;
+      Wheels->left = 0.0;
+      Wheels->right = 0.0;
+      return Wheels;
     }
 
 
