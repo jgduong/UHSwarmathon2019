53a1cfee3daa4d52329caf52b0290023f1a82199
jgduong <jennyduong1996@gmail.com>
2019-03-06 19:10:57 -0600

    Update SpiralSearchController.h

--- src/behaviours/src/SpiralSearchController.h
+++ src/behaviours/src/SpiralSearchController.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <set>
-#include "LogicController.h"
+//#include "LogicController.h"
 using namespace std;
 
 struct wheels {
@@ -32,24 +32,24 @@ class SpiralSearchController {
     }
 
     bool isVisited(float x, float y, const unordered_map<float, set<float>> &visitedLocations) {
-    if (visitedLocations.find(x) != visitedLocations.end()) {
-      //x location exists in hashmap, check y coordinate
-      if (visitedLocations[x].find(y) != visitedLocations[x].end()) {
-        //y location also exists, so this coordinate has been visited
-        return true;
-      }
-      else {
-          return false;	
+      if (visitedLocations.find(x) != visitedLocations.end()) {
+        //x location exists in hashmap, check y coordinate
+        if (visitedLocations[x].find(y) != visitedLocations[x].end()) {
+          //y location also exists, so this coordinate has been visited
+          return true;
         }
-      }
-      else {
-        return false;
-      }
+        else {
+            return false;	
+          }
+        }
+        else {
+          return false;
+        }
     }
     
     struct wheels DoWork(const unordered_map<float, set<float>> &visitedLocations) {
       cout << "test: current location (" << currX << ", " << currY << ")";
-      if (isVisited(currX, currY, visitedLocations)
+      if (isVisited(currX, currY, visitedLocations))
       {
         cout << "exists in the hashmap" << endl;
       }
