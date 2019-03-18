f631faf0779e46750e3df864074ac85a76b50486
jgduong <jennyduong1996@gmail.com>
2019-03-18 16:37:05 -0500

    Create PickupController.h

---
+++ src/behaviours/src/PickupController.h
@@ -0,0 +1,20 @@
+#include "Wheels.h"
+#include <iostream>
+using namespace std;
+
+class PickupController {
+
+private:
+
+public: 
+
+    wheels Wheels;
+    
+    wheels DoWork() {
+      cout << "Target detected : in PICKUP state" << endl;
+      Wheels->left = 0.0;
+      Wheels->right = 0.0;
+    }
+
+
+};
