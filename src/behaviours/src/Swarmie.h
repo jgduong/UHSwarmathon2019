cf204bbbc15b67875edeefae1392b5c3a0df3c8a
jgduong <jennyduong1996@gmail.com>
2019-03-19 12:42:27 -0500

    Update and rename Wheels.h to Swarmie.h

---
+++ src/behaviours/src/Swarmie.h
@@ -0,0 +1,11 @@
+#ifndef SWARMIE_H
+#define SWARMIE_H
+
+struct Swarmie {
+  float left;
+  float right;
+  float wrist;
+  float finger;
+};
+
+#endif 
--- src/behaviours/src/Wheels.h
+++
@@ -1,9 +0,0 @@
-#ifndef WHEELS_H
-#define WHEELS_H
-
-struct wheels {
-  float left;
-  float right;
-};
-
-#endif 
