ac76a0f655ec4e39e1d6f9b2ce61ca130baf8e2b
jgduong <jennyduong1996@gmail.com>
2019-03-20 18:44:14 -0500

    Update DropoffController.h

--- src/behaviours/src/DropoffController.h
+++ src/behaviours/src/DropoffController.h
@@ -134,7 +134,8 @@ class DropoffController {
 				  cout << "Made it to home base...dropping off cube!" << endl;
 	  			  //fngr.data = M_PI_2;
 				  //fingerAnglePublish.publish(fngr);
-				  swarmie.finger = M_PI_2 + 0.5;
+				  //swarmie.finger = M_PI_2 + 0.5;
+				  swarmie.finger = M_PI;
 				  //sendDriveCommand(0.0, 0.0);
 				  swarmie.left = 0.0;
 				  swarmie.right = 0.0;
