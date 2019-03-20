5b4c3892448d258cb792ee21e18f161bf3472855
jgduong <jennyduong1996@gmail.com>
2019-03-20 18:36:00 -0500

    Update ROSAdapter.cpp

--- src/behaviours/src/ROSAdapter.cpp
+++ src/behaviours/src/ROSAdapter.cpp
@@ -540,7 +540,7 @@ void targetHandler(const apriltags_ros::AprilTagDetectionArray::ConstPtr& messag
 	    }
 	    
 	    //logicController.SetAprilTags(tags);
-		if (currState == SPIRAL_SEARCH && ) {	
+		if (currState == SPIRAL_SEARCH) {	
 			currState = PICKUP;
 			//swarmie.pickupSuccess = false;
 			logicController->updateTags(tags.back().getPositionX(), tags.back().getPositionY(), tags.back().getPositionZ());
