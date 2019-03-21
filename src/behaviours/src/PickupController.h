28a110338fac38cc53a5cc5481af8d06fe4fabd7
jgduong <jennyduong1996@gmail.com>
2019-03-21 13:39:53 -0500

    Update PickupController.h

--- src/behaviours/src/PickupController.h
+++ src/behaviours/src/PickupController.h
@@ -31,7 +31,7 @@ public:
     
     
 	void updateTags(vector<Tag> tagsReceived) {
-	    tagZ = tags[indexOfClosestTag].getPositionZ()
+	    tagZ = tags[indexOfClosestTag].getPositionZ();
 	    minX = tagsReceived[0].getPositionX();
 	    for (int i = 0; i < tagsReceived.size(); i++)
 	    {
