b08b5df676318934687b4ff0163820f777bad931
jgduong <jennyduong1996@gmail.com>
2019-03-21 19:00:44 -0500

    Update ObstacleController.h

--- src/behaviours/src/ObstacleController.h
+++ src/behaviours/src/ObstacleController.h
@@ -4,10 +4,40 @@ using namespace std;
 
 
 class ObstacleController {
-
-  private: 
   
   public:
+    //default to high numbers (no obstacles)
+    float leftSonar = 10.0;
+    float rightSonar = 10.0;
+    float centerSonar = 10.0;
+    bool init = true;
+    Swarmie swarmie;
+    swarmi.pickupSuccess = false;
+    swarmie.dropoffSuccess = false;
+  
+    void updateSonar(float left, float center, float right) {
+      leftSonar = left;
+      rightSonar = right; 
+      centerSonar = center;
+    }
+  
+    void DoWork() {
+      
+      if (sonarLeft <= 0.5) {
+        cout << "Obstacle on the left!" << endl;
+        //swerve right
+        swarmie.left = 100.0;
+        swarmie.right = 60.0;
+      }
+      else if (sonarRight <= 0.5) {
+        //swerve left
+        cout << "Obstacle on the right!" << endl;
+        swarmie.right = 100.0;
+        swarmie.left = 60.0;
+      }
+      
+      
+    }
 
 
 };
