5ec7babfb79685342890837a60d3efcc29cb90f9
Jordan Perez <43359490+jl-perez@users.noreply.github.com>
2019-03-21 23:19:09 -0500

    Update ROSAdapter.cpp

--- src/behaviours/src/ROSAdapter.cpp
+++ src/behaviours/src/ROSAdapter.cpp
@@ -597,7 +597,7 @@ void sonarHandler(const sensor_msgs::Range::ConstPtr& sonarLeft, const sensor_ms
 		currState = AVOID_OBSTACLE;
 		logicController->UpdateSonar(sonarLeftData, sonarCenterData, sonarRightData);
 	}
-	if ((sonarLeftData <= 0.75 || sonarCenterData <= 0.75 || sonarRightData <= 0.75) && (currState == DROPOFF && (logicController->dropoffController.initCalc || logicController->dropoffController.driveToHome || backToSpiral) ))
+	if ((sonarLeftData <= 0.75 || sonarCenterData <= 0.75 || sonarRightData <= 0.75) && (currState == DROPOFF && (logicController->dropoffController.initCalc || logicController->dropoffController.driveToHome || logicController->dropoffController.backToSpiral) ))
 	{
 		prevState = currState;
 		currState = AVOID_OBSTACLE;
