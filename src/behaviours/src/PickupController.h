2b1523ae2dc548b7e4432e38c2e8260c73a74e38
jgduong <jennyduong1996@gmail.com>
2019-04-02 07:21:23 -0500

    Update PickupController.h

--- src/behaviours/src/PickupController.h
+++ src/behaviours/src/PickupController.h
@@ -151,21 +151,21 @@ public:
 		    swarmie.right = 0.0;
 		 swarmie.wrist = 1.25;
 		 swarmie.finger = M_PI_2 + 0.2;
-		 if ( minX > -0.025 )
+		 if ( minX > -0.02 )
 		    {
 			//sendDriveCommand(6.0, -5.0);
 			swarmie.left = 5.0;
 			swarmie.right = -6.0;
 			 cout << "calibrating by rotating right... " << endl;
 		    }
-		    else if ( minX < -0.035 )
+		    else if ( minX < -0.025 )
 		    {
 			//sendDriveCommand(-5.0, 7.0);
 			swarmie.left = -6.0;
 			swarmie.right = 5.0;
 			    cout << "calibrating by rotating left... " << endl;
 		    }   
-		    else if (minX <= -0.025 && minX >= -0.035)
+		    else if (minX <= -0.020 && minX >= -0.025)
 		    {
 			    approachCube = false;
 			    approachCube2 = true;
