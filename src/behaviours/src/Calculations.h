c37d2226118d9a56efb0d8b58d13ccaf149f72ce
jgduong <jennyduong1996@gmail.com>
2019-04-02 16:44:20 -0500

    Update Calculations.h

--- src/behaviours/src/Calculations.h
+++ src/behaviours/src/Calculations.h
@@ -25,4 +25,23 @@ float calcDistance(float curX, float curY, float goalX, float goalY) {
 	return dist;
 }
 
+bool isVisited(float x, float y, unordered_map<float, set<float>> &visitedLocations) {
+	float normX = normalizedValue(x);
+	float normY = normalizedValue(y);
+     
+	if (visitedLocations.find(normX) != visitedLocations.end()) {
+        //x location exists in hashmap, check y coordinate
+		if (visitedLocations[normX].find(normY) != visitedLocations[normX].end()) {
+		//y location also exists, so this coordinate has been visited
+		  	return true;
+        	}
+        	else {
+            		return false;	
+          	}
+        }
+        else {
+          	return false;
+        }
+}
+
 #endif
