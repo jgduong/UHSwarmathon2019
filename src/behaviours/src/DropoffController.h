2ba4ab82b821ace36c99d27358802e37a96e26bb
Jordan Perez <43359490+jl-perez@users.noreply.github.com>
2019-04-04 19:53:09 -0500

    Update DropoffController.h

--- src/behaviours/src/DropoffController.h
+++ src/behaviours/src/DropoffController.h
@@ -325,6 +325,7 @@ class DropoffController {
 			  	turnSize = desiredTheta - currTheta;
 			  }
 			  */
+			  turnSize = desiredTheta - currTheta;
 			cout << "turnSize here is: " << turnSize << endl;
 			bool exceedMag = false;
 			//ninetyRotate = currTheta;
