af608d101f63fd96e711e569dea7e3d97211131c
Jordan Perez <43359490+jl-perez@users.noreply.github.com>
2019-04-17 17:53:20 -0500

    Update ROSAdapter.cpp

--- src/behaviours/src/ROSAdapter.cpp
+++ src/behaviours/src/ROSAdapter.cpp
@@ -488,7 +488,7 @@ void behaviourStateMachine(const ros::TimerEvent&)
 		//}
 
 		
-		if (currState == PICKUP && swarmie.left == 5.0 && swarmie.right == 5.0) {
+		if (currState == PICKUP && swarmie.left == 5.0 && swarmie.right == 5.0 && tags.size() == 0) {
 		    //centering on tag has failed (timeout), return to spiral search
 		    /*currState = SPIRAL_SEARCH;
 			prevState = PICKUP;*/
